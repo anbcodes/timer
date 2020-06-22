5916404573d856beacc4ef1b90fba8ba8dcf9b37
Andrew Baumes <andrewbaumes@gmail.com>
2020-06-22 09:06:21 -0400

    myapp

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/essential',
+    '@vue/airbnb',
+  ],
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    "no-use-before-define": ["error", { "functions": false, "classes": false }]
+  },
+};
