dfae5bee0d1061a2d4498102e8c2e821a3982d83
Andrew Baumes <andrewbaumes@gmail.com>
2020-06-23 12:12:15 -0400

    Rounded seconds

--- src/main/main.js
+++ src/main/main.js
@@ -77,7 +77,7 @@ ipcMain.handle('settingsOpened', () => {
 function formatTime(secNum) {
   let hours = Math.floor(secNum / 3600);
   let minutes = Math.floor((secNum - (hours * 3600)) / 60);
-  let seconds = secNum - (hours * 3600) - (minutes * 60);
+  let seconds = (secNum - (hours * 3600) - (minutes * 60)).toFixed(2);
 
   if (hours < 10) { hours = `0${hours}`; }
   if (minutes < 10) { minutes = `0${minutes}`; }
