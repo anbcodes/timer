9a02baa8f2897ac02d074d34fe5dd580c46c02c8
anbcodes <anbcodes@protonmail.com>
2020-06-24 11:05:45 -0400

    Debugging electron action

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    "no-use-before-define": ["error", { "functions": false, "classes": false }]
+    "no-use-before-define": ["error", { "functions": false, "classes": false }],
+    "linebreak-style": 'off',
   },
 };
